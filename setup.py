f5a81d40233bc6d39193d43b92783934fbb14c93
raphaelrpl <raphael.wcosta@gmail.com>
2020-02-26 14:17:09 -0300

    Review repository structure

--- setup.py
+++ setup.py
@@ -4,21 +4,41 @@ import os
 from setuptools import find_packages, setup
 
 
-tests_require = [
-    'pytest>=5.0.0,<6.0.0',
+docs_require = [
+    'bdc-readthedocs-theme @ git+git://github.com/brazil-data-cube/bdc-readthedocs-theme.git#egg=bdc-readthedocs-theme',
+    'Sphinx>=2.1.2',
 ]
 
+tests_require = [
+    'coverage>=4.5',
+    'coveralls>=1.8',
+    'pytest>=5.2',
+    'pytest-cov>=2.8',
+    'pytest-pep8>=1.0',
+    'pydocstyle>=4.0',
+    'isort>4.3',
+    'sqlalchemy-diff>=0.1.3',
+    'alembic-verify>=0.1.4',
+    'check-manifest>=0.40'
+]
 
 extras_require = {
-    "docs": [
-        'bdc-readthedocs-theme @ git+git://github.com/brazil-data-cube/bdc-readthedocs-theme.git#egg=bdc-readthedocs-theme',
-        'Sphinx>=2.1.2',
-    ],
-    "tests": tests_require
+    'docs': docs_require,
+    'tests': tests_require,
 }
 
+extras_require['all'] = [ req for exts, reqs in extras_require.items() for req in reqs ]
+
+setup_requires = [
+    'pytest-runner>=5.2',
+]
+
+install_requires = [
+    'lccs-db @ git+git://github.com/brazil-data-cube/lccs-db.git#egg=lccs-db'
+]
+
 g = {}
-with open(os.path.join('bdc_sample', 'manifest.py'), 'rt') as fp:
+with open(os.path.join('bdc_sample', 'version.py'), 'rt') as fp:
     exec(fp.read(), g)
     version = g['version']
 
@@ -30,22 +50,9 @@ setup(
     author_email='admin@admin.com',
     url='https://github.com/brazil-data-cube/sampledb.git',
     packages=find_packages(),
-    install_requires=[
-        'geopandas>=0.5.0',
-        'gdal>=2.3.3,<3',
-        'SQLAlchemy[postgresql]>=1.3.4',
-        'alembic>=1.0.10',
-        'GeoAlchemy2>=0.6.3',
-        'Shapely>=1.6.4',
-        'Flask>=1.1.1',
-        'Flask-Cors>=3.0.8',
-        'flask-restplus>=0.13.0',
-        'Flask-Script>=2.0.6',
-        'flask_bcrypt>=0.7.1',
-        'marshmallow-sqlalchemy>=0.19.0',
-        'bdc-core @ git+git://github.com/brazil-data-cube/bdc-core.git#egg=bdc-core'
-    ],
     extras_require=extras_require,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
     tests_require=tests_require,
     include_package_data=True,
 )
